3efc8da656c1370381ad4f998fd9c0d8888595bf
Unique-work <19805062401@163.com>
2021-12-15 17:31:54 +0800

    实体类(orderdetail)

---
+++ src/com/dfgsd/model/OrderDetail.java
@@ -0,0 +1,31 @@
+package com.dfgsd.model;
+
+public class OrderDetail {
+    int oid;
+    int pid;
+    int num;
+
+    public int getOid() {
+        return oid;
+    }
+
+    public void setOid(int oid) {
+        this.oid = oid;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
