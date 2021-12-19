100338bbfd7f980a16138fb4695e5e38b9201ae9
Unique-work <19805062401@163.com>
2021-12-19 15:03:21 +0800

    操作类(orderdo)-添加3-dev

--- src/com/dfgsd/impl/Orderimpl.java
+++ src/com/dfgsd/impl/Orderimpl.java
@@ -1,8 +1,20 @@
 package com.dfgsd.impl;
 
 import com.dfgsd.DAO.DAO;
+import com.dfgsd.model.Order;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class Orderimpl {
     DAO dao=new DAO();
+    private PreparedStatement ps;
+    private ResultSet rs;
+    String sql;
+    public  boolean insert(Order order){
+
+        return false;
+    }
+
 }
 
